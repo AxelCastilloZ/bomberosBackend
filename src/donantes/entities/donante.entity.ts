6aca421e38128e53212cdce73076760a23e70f08
crissXcoder <cristhian.altamirano.montes@est.una.ac.cr>
2025-06-02 02:19:39 -0600

    Agreando el CRUD de Donantes desde la base de datos

--- src/donantes/entities/donante.entity.ts
+++ src/donantes/entities/donante.entity.ts
@@ -1,7 +1,19 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity('donantes')
 export class Donante {
+  @PrimaryColumn()
   id: string;
+
+  @Column()
   nombre: string;
+
+  @Column({ type: 'text' })
   descripcion: string;
+
+  @Column()
   logo: string;
+
+  @Column()
   url: string;
 }
